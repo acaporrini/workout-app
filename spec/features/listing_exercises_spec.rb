099ca8faa4482f99d976aa89c7fcc24cc406a7cc
acaporrini <a.caporrini@gmail.com>
2016-02-20 11:31:17 +0100

    implement display friends feature

--- spec/features/listing_exercises_spec.rb
+++ spec/features/listing_exercises_spec.rb
@@ -4,10 +4,14 @@ RSpec.feature "Listing Exercise" do
   before do
     @john = User.create!(email: "john@example.com",
     password: "password" ,first_name: "John", last_name:"Doe")
+    @sarah = User.create!(email: "sarah@example.com",
+    password: "password" ,first_name: "Sarah", last_name:"Doe")
     login_as(@john)
     @e1 = @john.exercises.create(duration_in_min: 20, workout: "Body building routine", workout_date: Date.today)
     @e2 = @john.exercises.create(duration_in_min: 20, workout: "Cardio routine", workout_date: Date.today)
+    @following = Friendship.create(user: @john, friend: @sarah)
   end
+
   scenario "Shows user's workout for last 7 days" do
     visit "/"
 
@@ -18,5 +22,16 @@ RSpec.feature "Listing Exercise" do
     expect(page).to have_content(@e2.duration_in_min)
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
+  end
+
+  scenario "Shows a list of user's friends" do
+    visit "/"
+
+    click_link "My Lounge"
+
+    expect(page).to have_content("My Friends")
+    expect(page).to have_link(@sarah.full_name)
+    expect(page).to have_link("Unfollow")
+
   end
 end
